2f0207e03c0fc989aa79f6fc6c00123c314d8df4
Chris Wu <79438125+Teostradragon@users.noreply.github.com>
2022-08-19 16:53:44 +1000

    Push

--- todolist/src/main.js
+++ todolist/src/main.js
@@ -5,8 +5,8 @@ import vuetify from "./plugins/vuetify";
 import "material-design-icons-iconfont/dist/material-design-icons.css";
 import axios from 'axios'
 import store from './store'
-import Charts from './views/lib/charts'
-Vue.use(Charts)
+
+
 
 
 Vue.prototype.$axios = axios;
